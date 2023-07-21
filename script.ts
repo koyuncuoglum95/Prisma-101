57692af53c67bdc6ee93f3c47c7d4349f14c7d21
koyuncuoglum95 <koyuncuoglum@outlook.com>
2023-07-20 21:47:59 -0700

    Connecting Postgresql

--- script.ts
+++ script.ts
@@ -3,51 +3,26 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-  // ... you will write your Prisma Client queries here
-
-  /* Create a new User record
-  const user = await prisma.user.create({
+  await prisma.user.create({
     data: {
-        name: 'Frank',
-        email: 'FrankJohnson@prisma.io'
-    },
-  })
-  console.log(user);
-  */
-
-
-  /* Retrieve all User records
-  const users = await prisma.user.findMany();
-  console.log(users);
- */
-
-
-  /*  Explore relation queries with Prisma
-  const user = await prisma.user.create({
-    data: {
-      name: 'Bob',
-      email: 'bob@prisma.io',
+      name: 'Alice',
+      email: 'alice@prisma.io',
       posts: {
-        create: {
-          title: 'Hello Bitches',
-        },
+        create: { title: 'Hello World' },
+      },
+      profile: {
+        create: { bio: 'I like turtles' },
       },
     },
   })
-  console.log(user)
-  */
 
-
-
-  /*
-  const usersWithPosts = await prisma.user.findMany({
+  const allUsers = await prisma.user.findMany({
     include: {
       posts: true,
+      profile: true,
     },
   })
-  console.dir(usersWithPosts, { depth: null })
-*/
-
+  console.dir(allUsers, { depth: null })
 }  
 
 
